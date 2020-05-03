74a2c5ea98e36456ae5c33808e7f8bb1bdcfa1b6
shakow <david.jacquelin@outlook.com>
2020-05-03 23:16:37 +0200

    aze

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/"><img class="icon-netfilm" src="../src/assets/netfilm.svg"></router-link>
     </div>
     <router-view/>
   </div>
