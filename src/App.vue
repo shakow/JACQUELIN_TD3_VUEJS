b39e20793db91c87afe3fd5fd286277f4702db5d
shakow <david.jacquelin@outlook.com>
2020-05-04 01:20:37 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/"><img class="icon-netfilm" src="../src/assets/netfilm.svg"></router-link>
+      <router-link to="/"><img class="icon-netfilm" src="../src/assets/icon-netflim.svg"></router-link>
     </div>
     <router-view/>
   </div>
